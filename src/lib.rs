a2d8deeb417e63f552f6393bbdf3832084dcea2f
Maybe Waffle <waffle.lapkin@gmail.com>
2021-09-21 14:47:50 +0300

    Add `TakeOwnCell`

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
-//! This crate provides a new cell-like type, `TakeCell`. A `TakeCell` may
-//! store arbitrary non-`Copy` types, can be read from at most once and provides
-//! direct unique access to the stored contents. The core API looks roughly like
-//! this (and there’s much more inside, read on!):
+//! This crate provides two new cell-like types, `TakeCell` and `TakeOwnCell`.
+//! Both may store arbitrary non-`Copy` types, can be read from at most once and
+//! provide direct unique access to the stored contents. The core API looks
+//! roughly like this (and there’s much more inside, read on!):
 //! ```rust,ignore
 //! impl<T> TakeCell<T> {
-//!     const fn new() -> OnceCell<T> { ... }
+//!     const fn new(v: T) -> Self { ... }
 //! }
 //! impl<T: ?Sized> TakeCell<T> {
 //!     fn get(&self) -> Option<&mut T> { ... }
 //! }
+//!
+//! impl<T> TakeOwnCell<T> {
+//!     const fn new(v: T) -> Self { ... }
+//!     fn get(&self) -> Option<T> { ... }
+//! }
 //! ```
 //! Note that, like with `RefCell` and `Mutex`, the `take` method requires only
 //! a shared reference. Because of the single read restriction `take` can
-//! return a `&mut T` instead of `RefMut<T>` or `MutexGuard<T>`. In some sense
-//! `TakeCell` can be thought as a `Mutex` without unlocking (or rather with
-//! unlocking requiring unique access to the `Mutex`, see [`heal`]).
+//! return a `&mut T` or `T` instead of `RefMut<T>` or `MutexGuard<T>`. In some
+//! sense `TakeCell` can be thought as a `Mutex` without unlocking (or rather
+//! with unlocking requiring unique access to the `Mutex`, see [`heal`]).
 //!
 //! [`heal`]: TakeCell::heal
 //!
@@ -99,6 +104,7 @@
 #![no_std]
 use core::{
     cell::UnsafeCell,
+    mem::ManuallyDrop,
     sync::atomic::{AtomicBool, Ordering},
 };
 
@@ -269,8 +275,200 @@ impl<T> From<T> for TakeCell<T> {
 /// This is again similar to a `Mutex`.
 unsafe impl<T: ?Sized + Send> Sync for TakeCell<T> {}
 
-// TODO: There may be a slightly different cell, which returns an owned value
-// instead of a reference (ie optimized version of `TakeCell<Option<T>>`)
+/// A cell type which value can be taken only once.
+///
+/// In difference with [`TakeCell`](crate::TakeCell), this type provides
+/// ownership, and not a reference to the inner value. Because of this it can't
+/// contain unsized values.
+///
+/// See [crate-level documentation](mod@self) for more.
+#[derive(Default)]
+pub struct TakeOwnCell<T>(TakeCell<ManuallyDrop<T>>);
+
+impl<T> TakeOwnCell<T> {
+    /// Creates a new `TakeOwnCell` containing the given value.
+    pub const fn new(v: T) -> Self {
+        Self(TakeCell::new(ManuallyDrop::new(v)))
+    }
+
+    /// Returns the underlying value.
+    ///
+    /// After this function once returns `Some(_)` all consequtive calls before
+    /// [`heal`] will return `None` as the value is already taken.
+    ///
+    /// [`heal`]: TakeOwnCell::heal
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use takecell::TakeOwnCell;
+    /// let cell = TakeOwnCell::new(17);
+    ///
+    /// let value: i32 = cell.take().unwrap();
+    /// assert_eq!(value, 17);
+    ///
+    /// // Already taken
+    /// assert!(cell.take().is_none());
+    /// assert!(cell.into_inner().is_none());
+    /// ```
+    pub fn take(&self) -> Option<T> {
+        self.0
+            .take()
+            // ## Safety
+            //
+            // `TakeCell` guatantees that unique reference to the underlying value is returned only
+            // once before `TakeCell::heal`. We ensure a new value is emplaced if it was taken
+            // before calling `TakeCell::heal`.
+            //
+            // In all other places (like `drop` and `get`) we check if the value was taken.
+            //
+            // This guarantees that the value is not duplicated.
+            .map(|value| unsafe { ManuallyDrop::take(value) })
+    }
+
+    /// Returns `true` if the underlying value has been already [`take`]n.
+    ///
+    /// ie if this function returns `true`, then [`take`] will return `None`.
+    /// Note however that the oposite is not true: if this function returned
+    /// `false` it doesn't guarantee that [`take`] will return `Some(_)` since
+    /// there may have been concurent calls to [`take`].
+    ///
+    /// [`take`]: TakeOwnCell::take
+    pub fn is_taken(&self) -> bool {
+        self.0.is_taken()
+    }
+
+    /// Returns a mutable reference to the underlying data.
+    ///
+    /// This call borrows `TakeOwnCell` mutably (at compile-time) which
+    /// guarantees that we possess the only reference.
+    ///
+    /// Note that this function does not affect the [`take`]. ie [`take`] may
+    /// still return `Some(_)` after a call to this function. The oposite is not
+    /// true, after the value is [`take`]n this function returns `None`.
+    ///
+    /// [`take`]: TakeOwnCell::take
+    pub fn get(&mut self) -> Option<&mut T> {
+        match self.is_taken() {
+            false => {
+                // ## Safety
+                //
+                // While this code doesn't use `unsafe{}` it can be affected by other unsafe
+                // blocks (see: `take`).
+                //
+                // The value may only be accessed if it was not taken before.
+                Some(&mut *self.0.get())
+            }
+            true => None,
+        }
+    }
+
+    /// Unwraps the underlying value.
+    pub fn into_inner(self) -> Option<T> {
+        self.take()
+    }
+
+    /// Heal this cell. After a call to this function next call to [`take`] will
+    /// succeed again, even if [`take`] was called before.
+    ///
+    /// Returns a reference to the underlying value and `Err(v)` if this cell
+    /// was not taken before the call to this function.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use takecell::TakeOwnCell;
+    /// let mut cell = TakeOwnCell::new(17);
+    ///
+    /// let (uref, res) = cell.heal(12);
+    /// assert_eq!(res, Err(12));
+    /// assert_eq!(*uref, 17);
+    /// *uref = 0xAA;
+    ///
+    /// assert_eq!(cell.take(), Some(0xAA));
+    ///
+    /// let (uref, res) = cell.heal(12);
+    /// assert!(res.is_ok());
+    /// assert_eq!(*uref, 12);
+    /// *uref = 0xBB;
+    ///
+    /// assert_eq!(cell.into_inner(), Some(0xBB));
+    /// ```
+    ///
+    /// [`take`]: TakeCell::take
+    pub fn heal(&mut self, v: T) -> (&mut T, Result<(), T>) {
+        // ## Safety
+        //
+        // While this code doesn't use `unsafe{}` it can be affected by other unsafe
+        // blocks (see: `take`).
+        //
+        // The value must be emplaced if it was previously taken, before healing the
+        // underlying cell.
+
+        let res = match self.0.is_taken() {
+            true => {
+                *self.0.get() = ManuallyDrop::new(v);
+                Ok(())
+            }
+            false => Err(v),
+        };
+
+        self.0.heal();
+
+        let mref = &mut *self.0.get();
+        (mref, res)
+    }
+
+    /// Unchecked version of [`take`].
+    ///
+    /// ## Safety
+    ///
+    /// Call to this function must be the first call to [`steal`] or [`take`]
+    /// after cell creation or [`heal`].
+    ///
+    /// [`take`]: TakeOwnCell::take
+    /// [`steal`]: TakeOwnCell::steal
+    /// [`heal`]: TakeOwnCell::heal
+    pub unsafe fn steal(&self) -> T {
+        // ## Safety
+        //
+        // Guaranteed by the caller
+        ManuallyDrop::take(self.0.steal())
+    }
+}
+
+impl<T> From<T> for TakeOwnCell<T> {
+    fn from(v: T) -> Self {
+        Self::new(v)
+    }
+}
+
+/// ## Safety
+///
+/// It is possible to pass ownership via `&TakeOwnCell`. As such,
+/// `TakeOwnCell<T>` may be `Sync` (`TakeOwnCell<T>: Send`) if and only if `T`
+/// is `Send`. Otherwise there may be UB, see [this example], adopted from
+/// sslab-gatech rust group.
+///
+/// [this example]: https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=c5add45a552290fc206fe2e9c768e03f
+///
+/// `Sync` on the other hand is not required because `TakeOwnCell`'s value is
+/// only accesible from one thread at a time.
+///
+/// This is again similar to a `Mutex`.
+unsafe impl<T: Send> Sync for TakeOwnCell<T> {}
+
+impl<T> Drop for TakeOwnCell<T> {
+    fn drop(&mut self) {
+        // Drop the underlying value, if the cell still holds it
+        if !self.0.is_taken() {
+            // ## Safety
+            //
+            // The value has not been taken, so it can be dropped
+            unsafe { ManuallyDrop::drop(self.0.get()) }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -338,5 +536,67 @@ mod tests {
         *uref += 1;
         assert_eq!(*uref, 2);
         assert_eq!(cell.into_inner(), 2);
+    }
+}
+
+#[cfg(test)]
+mod own_tests {
+    use crate::TakeOwnCell;
+
+    #[test]
+    fn it_works() {
+        let cell = TakeOwnCell::new(17);
+
+        assert_eq!(cell.take(), Some(17));
+
+        assert!(cell.take().is_none());
+        assert!(cell.into_inner().is_none());
+    }
+
+    #[test]
+    fn heal() {
+        let mut cell = TakeOwnCell::new(17);
+
+        let (uref, res) = cell.heal(12);
+        assert_eq!(res, Err(12));
+        assert_eq!(*uref, 17);
+        *uref = 0xAA;
+
+        assert_eq!(cell.take(), Some(0xAA));
+
+        let (uref, res) = cell.heal(12);
+        assert!(res.is_ok());
+        assert_eq!(*uref, 12);
+        *uref = 0xBB;
+
+        assert_eq!(cell.into_inner(), Some(0xBB));
+    }
+
+    #[test]
+    fn r#static() {
+        static CELL: TakeOwnCell<i32> = TakeOwnCell::new(42);
+
+        assert!(!CELL.is_taken());
+
+        assert_eq!(CELL.take(), Some(42));
+
+        assert!(CELL.is_taken());
+        assert!(CELL.take().is_none());
+    }
+
+    #[test]
+    fn steal_takes() {
+        let cell = TakeOwnCell::new(1);
+
+        assert!(!cell.is_taken());
+
+        // ## Safety
+        //
+        // There was no calls to take or steal before
+        assert_eq!(unsafe { cell.steal() }, 1);
+
+        assert!(cell.is_taken());
+        assert!(cell.take().is_none());
+        assert!(cell.into_inner().is_none());
     }
 }
